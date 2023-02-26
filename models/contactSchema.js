381bb7f51d907c83f5693f838c3ed358df4d30df
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2023-02-27 01:01:23 +0530

    first commit

---
+++ models/contactSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const constactSchema = mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("contacts", constactSchema);
